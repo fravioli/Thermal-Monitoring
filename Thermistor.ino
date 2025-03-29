91cc239295091749d56fa8162f5884776ce5f00f
Anna F <148503262+fravioli@users.noreply.github.com>
2025-03-29 12:02:59 -0700

    Update Thermistor.ino

--- Thermistor.ino
+++ Thermistor.ino
@@ -1,9 +1,11 @@
-#include <LiquidCrystal.h>
-
-int ThermistorPin = 0;
-int Vo;
-float R1 = 9920; //for 10k thermistor
-float logR2, R2, Tc, Tf, Tk, volt, res;
+#include <LiquidCrystal_I2C.h>
+int ThermistorPin1 = 0;
+int ThermistorPin2 = 1;
+int V1;
+int V2;
+float R01 = 9947; //actual ressitances of resistor 1 and 2
+float R02 = 9923;
+float logR2, logR1, R1,R2, Tc1, Tf1, Tk1, volt1,volt2,Tc2, Tf2, Tk2, res, Tc_avg;
 float c1 = 1.111270029e-03, c2 = 2.371239799e-04, c3 = 0.7519850730e-07;
 unsigned long current_time;
 unsigned long last_display;
@@ -11,15 +13,18 @@ unsigned long last_measurement_time;
 #define INTERVAL_MES 10
 #define DISPLAY_MES 500
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 void setup() {
-  lcd.begin(16,2);
+  
+  lcd.init();
+  lcd.backlight();
   Serial.begin(9600);
   analogReference(EXTERNAL);
   last_display = millis();
   last_measurement_time = millis();
-  Vo = analogRead(ThermistorPin);
+  V1 = analogRead(ThermistorPin1);
+  V2 = analogRead(ThermistorPin2); 
+
 }
 
 void loop() {
@@ -27,38 +32,49 @@ void loop() {
 
   if(current_time - last_measurement_time > INTERVAL_MES){
     last_measurement_time = current_time;
-    Vo = 0.9*Vo + 0.1*analogRead(ThermistorPin);  //supposed to better average readings 
-    //Vo = analogRead(ThermistorPin);
+    V1 = 0.9*V1 + 0.1*analogRead(ThermistorPin1);  //weighted smoothing factor
+    V2 = 0.9*V2 + 0.1*analogRead(ThermistorPin2);
   }
-
-  //weighted smoothing factor does not work as well when temp dec
     
-  //getting the resistance reading from thermistor
-  //R2 = R1 * ((1023.0 / (float)Vo) - 1.0);  
-  volt = Vo *(3.3 /1023.0);
-  R2 = (R1 * volt)/(3.3 - volt);
+  //getting the resistance reading from thermistor 
+  volt1 = V1 *(3.3/1023.0);
+  volt2 = V2 *(3.3/1023.0);
+  R1 = (R01 * volt1)/(3.3 - volt1);
+  R2 = (R02 * volt2)/(3.3 - volt2);
+  logR1 = log(R1);
   logR2 = log(R2);
+  
   //Steinhart-Hart Equation
-  Tk = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
-  Tc = Tk - 273.15;  //this is temp K to C
-  Tf = (Tc * 9.0)/ 5.0 + 32.0; //temp C to F
-  Serial.println(Vo);
-  Serial.println(R2);
-  Serial.println(volt);
-  Serial.println(Tc);
+  //first thermistor
+  Tk1 = (1.0 / (c1 + c2*logR1 + c3*logR1*logR1*logR1));
+  Tc1 = Tk1 - 273.15;  //this is temp K to C
+  Tf1 = (Tc1 * 9.0)/ 5.0 + 32.0; //temp C to F
+  //second thermistor
+  Tk2 = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  Tc2 = Tk2 - 273.15;  //this is temp K to C
+  Tf2 = (Tc2 * 9.0)/ 5.0 + 32.0; //temp C to F
 
   if((current_time - last_display) > DISPLAY_MES){
     last_display = current_time;
+    Serial.print("Time: ");
+    Serial.println(current_time);
+    Serial.print("Thermistor 1 Temp: ");
+    Serial.print(Tc1);
+    Serial.println("C");
+    Serial.print("Thermistor 2 Temp: ");
+    Serial.print(Tc2);
+    Serial.println("C");
+    Serial.println("//////////");
     lcd.clear();
-    lcd.print("Temp = ");
-    lcd.print(Tf); 
-    lcd.println(" F ");
+    lcd.print("Temp1 = ");
+    lcd.print(Tc1); 
+    lcd.println(" C ");
     lcd.setCursor(0,1);
-    lcd.print("Temp = ");
-    lcd.print(Tc);
+    lcd.print("Temp2 = ");
+    lcd.print(Tc2);
     lcd.println(" C ");
   }
+
   
-  //TO DO implement error checking 
 
 }
